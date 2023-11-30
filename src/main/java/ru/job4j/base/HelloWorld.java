3778be5c7a05735d6ad9025bf4088fe20bec0bba
mihaylinsergey <semi999@mail.ru>
2023-11-30 11:55:54 +0300

    Init

---
+++ src/main/java/ru/job4j/base/HelloWorld.java
@@ -0,0 +1,9 @@
+package ru.job4j.base;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Kotlin");
+        HelloWorldKt.main(args);
+    }
+}
