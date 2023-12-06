863bb155cf6598e1ff631690d61b2e3465592107
mihaylinsergey <semi999@mail.ru>
2023-12-06 10:19:38 +0300

    8. Переопределение. [#191436]

--- src/main/kotlin/ru/job4j/oop/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/Profession.kt
@@ -1,9 +1,57 @@
 package ru.job4j.oop
 
-open class Profession(val education : String)
+open class Profession(val education : String) {
 
-class Doctor(education : String, internship : String) : Profession(education)
+    open fun getName() : String {
+        return education
+    }
 
-class Programmer(education : String, language : String) : Profession(education)
+    open fun action() {
+        println("execute some actions")
+    }
+}
 
-class Manager(education : String, salary : String) : Profession(education)
+class Doctor(education : String, internship : String) : Profession(education) {
+
+    override fun getName() : String {
+        println("getName from Doctor")
+        return super.getName()
+    }
+
+    override fun action() {
+        super.action()
+        println("action from Doctor")
+    }
+}
+
+class Programmer(education : String, language : String) : Profession(education) {
+
+    override fun getName() : String {
+        println("getName from Programmer")
+        return super.getName()
+    }
+
+    override fun action() {
+        super.action()
+        println("action from Programmer")
+    }
+}
+
+class Manager(education : String, salary : String) : Profession(education) {
+
+    override fun getName() : String {
+        println("getName from Manager")
+        return super.getName()
+    }
+
+    override fun action() {
+        super.action()
+        println("action from Manager")
+    }
+}
+
+fun main() {
+    val profession = Doctor("Doctor", "hospital")
+    println(profession.getName())
+    profession.action()
+}
